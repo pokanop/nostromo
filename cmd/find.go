bebd0393a06561fdc2b69291c1b80aa5dfacce19
Sahel Jalal <sahel.jalal@icloud.com>
2019-09-14 11:10:06 -0700

    Remove boilerplate comments

--- cmd/find.go
+++ cmd/find.go
@@ -20,14 +20,4 @@ Searches for "name" in commands and substitutions and prints matches.`,
 
 func init() {
 	rootCmd.AddCommand(findCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// findCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// findCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
