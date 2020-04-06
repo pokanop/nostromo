8005dd00b7addcde93f0b87b54e1fc55ce11ea3f
Sahel Jalal <sahel.jalal@icloud.com>
2020-04-06 02:13:32 -0700

    Fix unit test build

--- shell/shell_test.go
+++ shell/shell_test.go
@@ -14,7 +14,7 @@ func TestValidLanguages(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ValidLanguages(); !reflect.DeepEqual(got, tt.want) {
+			if got := SupportedLanguages(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ValidLanguages() = %v, want %v", got, tt.want)
 			}
 		})
